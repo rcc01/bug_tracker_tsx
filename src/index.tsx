5999ff187f54791941dc8187e3e7be4358e91d07
Raul Contreras <raul.cappuccio@gmail.com>
2023-03-29 11:45:15 +0200

    errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,13 @@
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
